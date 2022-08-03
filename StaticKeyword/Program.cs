292190b0dde38f20baab2b5fbab96feca233d535
Tetyana Murzina <tetyanamur@gmail.com>
2022-08-03 18:20:21 -0500

    initial commit

---
+++ StaticKeyword/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StaticKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          var celcius =  TempConverter.CelciusToFarenheit(75);
+            Console.WriteLine($"{celcius}");
+            var farenheit = TempConverter.CelciusToFarenheit(30);
+            Console.WriteLine($"{farenheit}");
+        }
+    }
+}
---
+++ StaticKeyword/TempConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticKeyword
+{
+    public static class TempConverter
+    {
+        public static double FarenheitToCelcius(double farenheit)
+        {
+            return (farenheit - 32) / 1.8;
+
+        }
+        public static double CelciusToFarenheit(double celcius)
+        {
+            return (celcius * 9) / 5 + 32;
+        }
+    }
+}
